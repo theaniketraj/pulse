d911f9e80639ba6bf0d855d45527cb8973450fbc
theaniketraj <cs2258.diatm@gmail.com>
2025-11-30 20:55:37 +0530

    feat: Add message types enumeration and webview messaging function for Prometheus API integration

--- src/api.ts
+++ src/api.ts
@@ -62,3 +62,13 @@ export class PrometheusApi {
     }
   }
 }
+
+export enum MessageTypes {
+  TRIGGER_ALERT = 'TRIGGER_ALERT',
+  UPDATE_METRICS = 'UPDATE_METRICS',
+  UPDATE_LOGS = 'UPDATE_LOGS'
+}
+
+export function sendMessageToWebview(webview: vscode.Webview, message: any) {
+  webview.postMessage(message);
+}
